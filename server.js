18e79fbeac09458f569a135f3db1fa56615e87a0
Pasquier Guillaume <guillaume.pasquier@gfi.fr>
2019-04-09 10:06:43 +0200

    simplified open_browser calls

--- server.js
+++ server.js
@@ -56,17 +56,13 @@ app.listen(5555, function () {
 	console.log('LGTV http server is up to http://localhost:5555');
     
 	/*
-	 open_browser_at 8:30-35
+	 open_browser_at & set_mute at 8:30-35
 	 */	
 	var open_browser_at = schedule.scheduleJob('00 30-35 08 * * 1-5', function() {
 		// Connect to TV:
-		console.log("Started : " + new Date());
+		console.log("open_browser_at - Started : " + new Date());
 		lgtv.connect(CONFIG.lgtvip, function(err, response){
-		  if (!err) {
-			console.log("Connected");
-			console.log("open_browser_at...");
-						
-			// open_browser_at:
+		  if (!err) {			
 		    lgtv.open_browser_at(url_borne, function(err1, response){
 				if (!err1) {
 				  console.log("open_browser_at ok:" + JSON.stringify(response));
@@ -81,40 +77,13 @@ app.listen(5555, function () {
 	});
 	
 	/*
-	 Mute 1 at 8:45
-	 */
-	var mute_on = schedule.scheduleJob('00 45 08 * * 1-5', function() {
-		// Connect to TV:
-		console.log("Started : " + new Date());
-		lgtv.connect(CONFIG.lgtvip, function(err, response){
-			lgtv.get_status(function(err, response){
-				if (!err) {
-				  console.log("get status ok:" + JSON.stringify(response));
-				  var mute = response.payload.mute;
-				  console.log("Volume is " + response.payload.volume + " and Mute is " + mute);
-				  if (mute == false) {
-					  lgtv.set_mute(true);
-				  }
-				} else {
-				  console.log("get status err:" + JSON.stringify(response));
-				}
-				lgtv.disconnect();
-			});
-		});
-	});
-	
-	/*
-	 open_browser_at 14:00:00
+	 open_browser_at & set_mute at 14:00:00
 	 */	
 	var open_browser_at2 = schedule.scheduleJob('00 00 14 * * 1-5', function() {
 		// Connect to TV:
-		console.log("Started : " + new Date());
+		console.log("open_browser_at2 - Started : " + new Date());
 		lgtv.connect(CONFIG.lgtvip, function(err, response){
-		  if (!err) {
-			console.log("Connected");
-			console.log("open_browser_at...");
-			
-			// open_browser_at:
+		  if (!err) {			
 		    lgtv.open_browser_at(url_borne, function(err1, response){
 				if (!err1) {
 				  console.log("open_browser_at ok:" + JSON.stringify(response));
@@ -125,29 +94,6 @@ app.listen(5555, function () {
 			});
 		  }
 		  lgtv.disconnect();
-		});
-	});
-	
-	/*
-	 Mute 2 at 14:01
-	 */
-	var mute_on2 = schedule.scheduleJob('00 01 14 * * 1-5', function() {
-		// Connect to TV:
-		console.log("Started : " + new Date());
-		lgtv.connect(CONFIG.lgtvip, function(err, response){
-			lgtv.get_status(function(err, response){
-				if (!err) {
-				  console.log("get status ok:" + JSON.stringify(response));
-				  var mute = response.payload.mute;
-				  console.log("Volume is " + response.payload.volume + " and Mute is " + mute);
-				  if (mute == false) {
-					  lgtv.set_mute(true);
-				  }
-				} else {
-				  console.log("get status err:" + JSON.stringify(response));
-				}
-				lgtv.disconnect();
-			});
 		});
 	});
 	
@@ -156,15 +102,12 @@ app.listen(5555, function () {
 	 */
 	var turn_off = schedule.scheduleJob('00 30 19 * * 1-5', function() {
 		// Connect to TV:
-		console.log("Started : " + new Date());
+		console.log("turn_off - Started : " + new Date());
 		lgtv.connect(CONFIG.lgtvip, function(err, response){
 		  if (!err) {
-			console.log("Connected");
 			console.log("turn_off...");
-			
 		    lgtv.turn_off();
 		  }
-		  lgtv.disconnect();
 		});
 	});
 	
